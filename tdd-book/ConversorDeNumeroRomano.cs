5713904d08b1f636c805d13fabf8804cf75cab14
Bruno Moura <contato@brunosite.com>
2020-08-02 22:24:25 -0300

    Fizemos uma refatoração masi dinâmica, mas mesmo assim o teste falhou

--- tdd-book/ConversorDeNumeroRomano.cs
+++ tdd-book/ConversorDeNumeroRomano.cs
@@ -1,13 +1,23 @@
-﻿namespace tdd_book
+﻿using System.Collections.Generic;
+
+namespace tdd_book
 {
     public class ConversorDeNumeroRomano
     {
+        private static readonly Dictionary<string, int> Tabela = new Dictionary<string, int>()
+        {
+            {"I", 1 },
+            {"V", 5 },
+            {"X", 10 },
+            {"L", 50 },
+            {"C", 100 },
+            {"D", 500 },
+            {"M", 1000 },
+        };
+
         public int Converte(string numeroEmRomano)
         {
-            if (numeroEmRomano.Equals("I")) return 1;
-            if (numeroEmRomano.Equals("V")) return 5;
-
-            return 0;
+            return Tabela[numeroEmRomano];
         }
     }
 }
